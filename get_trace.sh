d7a5da65ac7e4ce6fd65336d47918b1b139f8436
jnwilliams <jnwills94@gmail.com>
2020-11-22 17:42:00 -0500

    Generate opt from trace

--- get_trace.sh
+++ get_trace.sh
@@ -9,6 +9,7 @@ lru_out="lru_trace"
 random_out="random_trace"
 
 trace=$1
+compute_belady=true
 
 #Compile and run Hawkeye
 echo "Compiling Hawkeye..."
@@ -21,6 +22,12 @@ echo "Format Hawkeye trace as CSV..."
 python convert_to_csv.py -i "$hawkeye_out.txt" -o "$hawkeye_out.csv"
 rm "$hawkeye_out.txt"
 
+if [ "$compute_belady" = true ]; then
+	echo "Include Belady Optimal Solution in trace..."
+	python trace_with_belady.py -i "$hawkeye_out.csv" -o "$hawkeye_out""_belady.csv"
+	rm "$hawkeye_out.csv"
+fi
+
 #Compile and run LRU
 echo "Compiling LRU..."
 g++ -Wall --std=c++11 -o lru-config1 $lru_path lib/config1.a
@@ -31,6 +38,12 @@ echo "Running LRU..."
 echo "Format LRU trace as CSV..."
 python convert_to_csv.py -i "$lru_out.txt" -o "$lru_out.csv"
 rm "$lru_out.txt"
+
+if [ "$compute_belady" = true ]; then
+	echo "Include Belady Optimal Solution in trace..."
+	python trace_with_belady.py -i "$lru_out.csv" -o "$lru_out""_belady.csv"
+	rm "$lru_out.csv"
+fi
 
 #Compile and run LRU
 echo "Compiling Random..."
@@ -43,4 +56,8 @@ echo "Format Random trace as CSV..."
 python convert_to_csv.py -i "$random_out.txt" -o "$random_out.csv"
 rm "$random_out.txt"
 
-
+if [ "$compute_belady" = true ]; then
+	echo "Include Belady Optimal Solution in trace..."
+	python trace_with_belady.py -i "$random_out.csv" -o "$random_out""_belady.csv"
+	rm "$random_out.csv"
+fi
---
+++ trace_with_belady.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+
+import argparse
+
+def belady( df ):
+    sets = 2048
+    ways = 16
+
+    cache = -1 * np.ones( ( sets, ways ), dtype=np.float64 )
+    cache_hist = -1 * np.ones( ( df.shape[0], ways ) )
+    
+    hit = np.zeros( df.shape[0] )
+    evict_way = -1 * np.ones( df.shape[0] )
+
+    for i in range( df.shape[0] ):
+        which_set = df['Set'][i]
+        p_addr = df['Physical Address'][i]
+
+        if p_addr in cache[which_set]:
+            hit[i] = 1
+        else:
+            bel = np.zeros( ways )
+            for j, way in enumerate( cache[which_set] ):
+                if way < 0:
+                    cache[which_set,j] = p_addr
+                    break
+
+                future_acc = np.where( df['Physical Address'][i+1:] == way )[0]
+
+                if len( future_acc ) == 0:
+                    bel[j] = np.inf
+                else:
+                    bel[j] = future_acc[0]
+
+                if all( bel > 0 ):
+                    next_access = np.where( df['Physical Address'][i+1:] == p_addr )[0]
+                    if len( next_access ) == 0:
+                        next_access = np.inf
+                    else:
+                        next_access = next_access[0]
+
+                    if any( next_access <= bel ):
+                        replace = np.argmax( bel )
+                        evict_way[i] = replace
+                        cache[which_set,replace] = p_addr
+
+                cache_hist[i,:] = cache[which_set].tolist()
+    
+    cache_hist = np.array( cache_hist )
+    cache_col_names = [ "Cache_Set_{}".format( i ) for i in range( 16 ) ]
+    
+    cache_df = pd.DataFrame()
+    for l, col in enumerate( cache_col_names ):
+        cache_df.insert( l, col, cache_hist[:,l] )
+
+    belady_evict = pd.DataFrame( evict_way, columns=['Belady Evict'] )
+    belady_hit = pd.DataFrame( hit, columns=['Belady Hit'] )
+
+    belady_df = pd.concat( [ belady_evict, belady_hit, cache_df ], axis=1 )
+    return belady_df
+
+def main( ):
+    parser = argparse.ArgumentParser(description='Input and output csv for finding optimal')
+    parser.add_argument('-i', type=str, help='input_filename' )
+    parser.add_argument('-o', type=str, help='output_filename' )
+
+    args = parser.parse_args()
+    file_in = args.i
+    file_out = args.o
+
+    with open( file_in, 'r+' ) as f:
+        trace = pd.read_csv( f )
+
+    belady_df = belady( trace )
+    out_trace = pd.concat( [ trace, belady_df ], axis=1 )
+
+    with open( file_out, 'w+' ) as f:
+        out_trace.to_csv( f )
+
+if __name__ == "__main__":
+    main()
+
+    
